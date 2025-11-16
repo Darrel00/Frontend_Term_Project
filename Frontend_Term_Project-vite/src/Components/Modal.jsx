59553e988842909d882f5960fa97e89139820c72
kyletria1 <kyleadriantria01@gmail.com>
2025-11-16 10:11:26 -0800

    add html to Modal.jsx

--- Frontend_Term_Project-vite/src/Components/Modal.jsx
+++ Frontend_Term_Project-vite/src/Components/Modal.jsx
@@ -2,9 +2,14 @@ import "./Modal.css";
 
 function Modal(){
     return (
-        <div>
-            <form>
-
+        <div className="cardModal">
+            <h1 className="modalTitle">Add New Card</h1>
+            <button className="saveButton">Save</button>
+            <form className="modalForm">
+                <h2 className="inputTitle">Front Side</h2>
+                <input className="textInput" type="text"></input>
+                <h2 className="inputTitle">Back Side</h2>
+                <input className="textInput" type="text"></input>
             </form>
         </div>
     );
